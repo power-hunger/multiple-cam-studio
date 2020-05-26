cf911363320af7ba2d54602b8c445308362aae5f
Konrads Buss <konrads.buss@gmail.com>
2020-05-26 22:42:49 +0300

    claen up

---
+++ data_filtration.py
@@ -0,0 +1,70 @@
+import numpy as np
+import json
+from scipy.spatial import transform
+
+CAM_SERIALS_N_DEGREES = {'950122060941': 0, '951422062948': -60, '951422060619': -120,
+               '951422063135': -180, '950122060940': -240, '951422061191': -300}
+
+
+def rotate_point(x, y, z, degrees):
+    vec = [x,y,z]
+
+    rotation_degrees = degrees
+    rotation_radians = np.radians(rotation_degrees)
+    rotation_axis = np.array([0, 1, 0])
+
+    rotation_vector = rotation_radians * rotation_axis
+    rotation = transform.Rotation.from_rotvec(rotation_vector)
+    rotated_vec = rotation.apply(vec)
+
+    return rotated_vec[0], rotated_vec[1], rotated_vec[2]
+
+
+def rotate_cam():
+    for cam_data in CAM_SERIALS_N_DEGREES:
+        print("Rotating camera: " + cam_data + " by " + str(CAM_SERIALS_N_DEGREES[cam_data]) + " degrees")
+
+        with open('data/' + cam_data + '.json') as f:
+            json_data = json.load(f)
+
+        rotated_json = []
+        for frame in range(len(json_data)):
+            dynamic_list = []
+            for point in range(len(json_data[frame]["skeletons"][0])):
+                x = json_data[frame]["skeletons"][0][point]["x"]
+                y = json_data[frame]["skeletons"][0][point]["y"]
+                z = json_data[frame]["skeletons"][0][point]["z"]
+                x_rotated, y_rotated, z_rotated = rotate_point(x, y, z, CAM_SERIALS_N_DEGREES[cam_data])
+
+                if "confidence" in json_data[frame]["skeletons"][0][point]:
+
+                    dynamic_list.append({"confidence": json_data[frame]["skeletons"][0][point]["confidence"],
+                                          "x": x_rotated,
+                                          "y": y_rotated,
+                                          "z": z_rotated})
+                else:
+                    dynamic_list.append({"confidence": -1.0,
+                                          "x": x_rotated,
+                                          "y": y_rotated,
+                                          "z": z_rotated})
+
+            rotated_json.append({"skeletons": [dynamic_list], "timestamp": json_data[frame]["timestamp"]})
+
+        json_dump = json.dumps(rotated_json)
+
+        with open('results/' + cam_data + '_rotated.json', 'w+') as the_file:
+            the_file.write(json_dump)
+
+
+def pick_top_point():
+    print("f")
+
+
+
+def main():
+    rotate_cam()
+    # pick_top_point()
+
+
+if __name__ == "__main__":
+    main()
---
+++ visualization.py
@@ -0,0 +1,122 @@
+# IMPORTS
+import numpy as np
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d.axes3d as p3
+import matplotlib.animation as animation
+import json
+
+CAM_SERIALS = ['950122060941', '951422062948', '951422060619', '951422063135', '950122060940', '951422061191']
+
+def generate_data(nbr_iterations, nbr_elements, cam_data):
+    """
+    Generates dummy data.
+    The elements will be assigned random initial positions and speed.
+    Args:
+        nbr_iterations (int): Number of iterations data needs to be generated for.
+        nbr_elements (int): Number of elements (or points) that will move.
+    Returns:
+        list: list of positions of elements. (Iterations x (# Elements x Dimensions))
+    """
+    dims = (3,1)
+
+    # Random initial positions.
+    gaussian_mean = np.zeros(dims)
+    gaussian_std = np.ones(dims)
+    start_positions = np.array(list(map(np.random.normal, gaussian_mean, gaussian_std, [nbr_elements] * dims[0]))).T
+
+    # Random speed
+    start_speed = np.array(list(map(np.random.normal, gaussian_mean, gaussian_std, [nbr_elements] * dims[0]))).T
+
+    # Computing trajectory
+    data = [start_positions]
+    for iteration in range(nbr_iterations):
+        previous_positions = data[-1]
+        new_positions = previous_positions + start_speed
+        data.append(new_positions)
+
+    with open('trial/trial2/' + cam_data + '.json') as f:
+        json_data = json.load(f)
+
+    data_list = []
+
+    for frame in range(len(json_data)):
+        dynamic_list = []
+        for point in range(len(json_data[frame]['skeletons'][0])):
+            dynamic_list.append([json_data[frame]['skeletons'][0][point]['x'],
+                                 json_data[frame]['skeletons'][0][point]['y'],
+                                 json_data[frame]['skeletons'][0][point]['z']])
+        data_list.append(dynamic_list)
+
+
+    new_list = []
+    for index in range(len(data_list)):
+        npa = np.asarray(data_list[index], dtype=np.float64)
+        new_list.append(npa)
+        print(data_list[index])
+
+
+    return new_list
+
+def animate_scatters(iteration, data, scatters):
+    """
+    Update the data held by the scatter plot and therefore animates it.
+    Args:
+        iteration (int): Current iteration of the animation
+        data (list): List of the data positions at each iteration.
+        scatters (list): List of all the scatters (One per element)
+    Returns:
+        list: List of scatters (One per element) with new coordinates
+    """
+    for i in range(data[0].shape[0]):
+        scatters[i]._offsets3d = (data[iteration][i,0:1], data[iteration][i,1:2], data[iteration][i,2:])
+    return scatters
+
+def main(data, cam_data, save=False):
+    """
+    Creates the 3D figure and animates it with the input data.
+    Args:
+        data (list): List of the data positions at each iteration.
+        save (bool): Whether to save the recording of the animation. (Default to False).
+    """
+
+    # Attaching 3D axis to the figure
+    fig = plt.figure()
+    ax = p3.Axes3D(fig)
+
+    # Initialize scatters
+    scatters = [ax.scatter(data[0][i, 0:1], data[0][i, 1:2], data[0][i, 2:], s=5) for i in range(data[0].shape[0])]
+
+    # Number of iterations
+    print(data)
+    iterations = len(data)
+
+    # Setting the axes properties
+    ax.set_xlim3d([-1.5, 1.5])
+    ax.set_xlabel('X')
+
+    ax.set_ylim3d([-1.5, 1.5])
+    ax.set_ylabel('Y')
+
+    ax.set_zlim3d([1.6, 3.6])
+    ax.set_zlabel('Z')
+
+    ax.set_title('3D Animated Scatter Example')
+
+    # Provide starting angle for the view.
+    ax.view_init(100, 90)
+
+    ani = animation.FuncAnimation(fig, animate_scatters, iterations, fargs=(data, scatters),
+                                       interval=50, blit=False, repeat=True)
+
+    if save:
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800, extra_args=['-vcodec', 'libx264'])
+        ani.save(cam_data + '_original.mp4', writer=writer)
+
+    plt.show()
+
+
+for cam_data in CAM_SERIALS:
+    print("Creating video for: " + cam_data)
+    data = generate_data(2, 1, cam_data)
+    main(data, cam_data, save=True)
